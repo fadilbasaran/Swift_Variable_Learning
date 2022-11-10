b22c5f598bd2abcfa639b6986b87e49046236abf
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-11-10 13:50:13 +0300

    Değişknler öğrenildi

--- VariableLearning/main.swift
+++ VariableLearning/main.swift
@@ -9,3 +9,49 @@ import Foundation
 
 print("Hello, World!")
 
+
+//Tam Sayılar için Int
+
+//Ondalıklı sayılar için de Float, Double
+//Metinsel için String
+
+
+//True And False for Bool statement used
+
+
+
+
+
+//Değişebilen değişkenleri var ifadesi ile tanımlama yapılır
+var d1:Int
+
+d1=6
+
+
+print(d1)
+
+var d2:Int=67//Tanımlerken değerleri de verilebilir
+
+var d3=54//Böyle bir tanımlama yapıldığında değerin tam sayı oluduğunu kendisi ifade ediyor
+
+
+
+var d4=5.4
+
+var d5:Float=5.6
+
+var s1:String="metinsel ifadeler bu alanda yazılabilirler"
+
+var s2="Böyle de tanımlana bilir"
+ 
+ 
+
+
+//Sabit ifadeleri is  let anahtar sözcüğü başına getirilerek tanımlanır
+
+
+let d9:Int=10//Değeri hiç bir zaman değişmiyor
+
+var d10:String="metin \" \r second satır"
+
+print(d10)
