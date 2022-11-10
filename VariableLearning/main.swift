fcf4aca1195b3376651c35644b6ed5024b8a5e3f
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-11-10 11:39:15 +0300

    Initial Commit

---
+++ VariableLearning/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  VariableLearning
+//
+//  Created by Logical Defense Yazılım Arge Savunma San.ve Tic.Ltd.Şti. on 10.11.2022.
+//
+
+import Foundation
+
+print("Hello, World!")
+
